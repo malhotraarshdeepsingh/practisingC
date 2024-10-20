8c0a2a7299cf584ec9850a92126d8ffc833dfc41
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2024-10-20 17:45:56 +0530

    fixes

--- pyq/mst/!!!swap.c
+++ pyq/mst/!!!swap.c
@@ -3,16 +3,17 @@
 int main() {
     char str[100], word1[20], word2[20], word3[20], rest[100];
     int i = 0, j = 0, word_count = 0;
+
     // Input string from the user
     printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin); 
-    // Variables to store wordss
-    word1[0] = '\0';
-    word2[0] = '\0';
-    word3[0] = '\0';
-    rest[0] = '\0';
+    scanf("%[^\n]", str);
+
+    // Initialize words and rest
+    word1[0] = word2[0] = word3[0] = rest[0] = '\0';
+    
     // Parse the string word by word
     for (i = 0, j = 0; str[i] != '\0'; i++) {
+        // If a space is encountered, finalize the current word
         if (str[i] == ' ' || str[i] == '\n') {
             word_count++;
             if (word_count == 1) { // Store first word
@@ -24,28 +25,32 @@ int main() {
             } else if (word_count == 3) { // Store third word
                 word3[j] = '\0';
                 j = 0;
-            } else if (word_count > 3) { // Remaining part of the string
-                strncat(rest, &str[i - j], j);
-                strcat(rest, " ");
-                j = 0;
+            } else {
+                rest[j++] = ' '; // Add space for remaining string
             }
         } else {
-            if (word_count == 0 || word_count == 1 || word_count == 2 || word_count == 3) {
-                if (word_count == 0) {
-                    word1[j++] = str[i];
-                } else if (word_count == 1) {
-                    word2[j++] = str[i];
-                } else if (word_count == 2) {
-                    word3[j++] = str[i];
-                }
+            // Store characters in respective words or remaining part
+            if (word_count == 0) {
+                word1[j++] = str[i];
+            } else if (word_count == 1) {
+                word2[j++] = str[i];
+            } else if (word_count == 2) {
+                word3[j++] = str[i];
             } else {
-                j++;
+                rest[j++] = str[i]; 
             }
         }
     }
-    if (j > 0 && word_count == 3) {
-        strncat(rest, &str[i - j], j);
+    
+    // Finalize the last word if necessary
+    if (j > 0) {
+        if (word_count == 2) {
+            word3[j] = '\0'; // If there were exactly 3 words
+        } else {
+            rest[j] = '\0'; // Null terminate the remaining part
+        }
     }
+    
     // Output the result with the second and third words swapped
     printf("Output: %s %s %s %s\n", word1, word3, word2, rest);
     return 0;
