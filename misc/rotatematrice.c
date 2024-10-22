844cf1e662a4e90bb3e25d1c4c768e1c02b6ed8b
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2024-10-22 17:55:10 +0530

    Create rotatematrice.c

---
+++ misc/rotatematrice.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define N 3
+
+int main() {
+    int matrix[N][N];
+    int i,j;
+
+    printf("Enter elements of the first matrix (3x3):\n");
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = i; j < N; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    // Reverse each row
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N / 2; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][N - 1 - j];
+            matrix[i][N - 1 - j] = temp;
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
