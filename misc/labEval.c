b53e79653e0266bef05fffb2057eda089a3bb276
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2024-12-11 15:05:48 +0530

    Update labEval.c

--- misc/labEval.c
+++ misc/labEval.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Enum to represent employee type
+typedef enum {
+    HOURLY,
+    SALARIED
+} EmployeeType;
+
 // Structure to store employee details
 typedef struct {
     char name[50];
     int id;
-    char type[10]; // "Hourly" or "Salaried"
+    EmployeeType type;
     double hourlyWage;
     int weeklyHours;
     double weeklySalary;
@@ -14,9 +20,9 @@ typedef struct {
 
 // Function to calculate salary
 double calculateSalary(Employee *emp) {
-    if (strcmp(emp->type, "Hourly") == 0) {
+    if (emp->type == HOURLY) {
         return emp->hourlyWage * emp->weeklyHours;
-    } else if (strcmp(emp->type, "Salaried") == 0) {
+    } else if (emp->type == SALARIED) {
         return emp->weeklySalary;
     }
     return 0;
@@ -41,20 +47,24 @@ int main() {
         scanf(" %[^\n]", employees[i].name);
         printf("ID: ");
         scanf("%d", &employees[i].id);
-        printf("Type (Hourly/Salaried): ");
-        scanf("%s", employees[i].type);
 
-        if (strcmp(employees[i].type, "Hourly") == 0) {
+        int typeInput;
+        printf("Type (0 for Hourly, 1 for Salaried): ");
+        scanf("%d", &typeInput);
+
+        if (typeInput == 0) {
+            employees[i].type = HOURLY;
             printf("Enter hourly wage: ");
             scanf("%lf", &employees[i].hourlyWage);
             printf("Enter weekly hours worked: ");
             scanf("%d", &employees[i].weeklyHours);
-        } else if (strcmp(employees[i].type, "Salaried") == 0) {
+        } else if (typeInput == 1) {
+            employees[i].type = SALARIED;
             printf("Enter weekly salary: ");
             scanf("%lf", &employees[i].weeklySalary);
         } else {
-            printf("Invalid type entered!\n");
-            i--; // Retry for the same employee
+            printf("Invalid type entered! Please re-enter details.\n");
+            i--;
         }
     }
 
@@ -64,12 +74,12 @@ int main() {
         printf("\nEmployee %d:\n", i + 1);
         printf("Name: %s\n", employees[i].name);
         printf("ID: %d\n", employees[i].id);
-        printf("Type: %s\n", employees[i].type);
+        printf("Type: %s\n", (employees[i].type == HOURLY) ? "Hourly" : "Salaried");
 
-        if (strcmp(employees[i].type, "Hourly") == 0) {
+        if (employees[i].type == HOURLY) {
             printf("Hourly Wage: %.2lf\n", employees[i].hourlyWage);
             printf("Weekly Hours: %d\n", employees[i].weeklyHours);
-        } else if (strcmp(employees[i].type, "Salaried") == 0) {
+        } else if (employees[i].type == SALARIED) {
             printf("Weekly Salary: %.2lf\n", employees[i].weeklySalary);
         }
 
@@ -77,7 +87,6 @@ int main() {
         printf("Calculated Salary: %.2lf\n", salary);
     }
 
-    // Free allocated memory
     free(employees);
 
     return 0;
